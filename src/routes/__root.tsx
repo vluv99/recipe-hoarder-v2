70c06d215923c044455ad1f7082cbe188effeb5a
vluv99 <v.luva99@gmail.com>
2026-03-21 16:54:50 +0100

    sidebar layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,31 +5,46 @@ import {
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
-    SidebarHeader,
+    SidebarHeader, SidebarMenuButton, SidebarMenuItem,
     SidebarProvider, SidebarTrigger
 } from "../components/ui/sidebar.tsx";
 import {TooltipProvider} from "@/components/ui/tooltip.tsx";
+import {HomeIcon, NotebookTabs} from "lucide-react";
 
 
 const RootLayout = () => (
     <TooltipProvider>
     <SidebarProvider>
-        <Sidebar>
-            <SidebarHeader />
+        <Sidebar collapsible="icon">
+            <SidebarHeader>
+                <div className='flex justify-end'>
+                    <SidebarTrigger />
+                </div>
+            </SidebarHeader>
             <SidebarContent>
                 <SidebarGroup>
-                    <Link to="/" className="[&.active]:font-bold">
-                        Home
-                    </Link>
-                    <Link to="/about" className="[&.active]:font-bold">
-                        About
-                    </Link>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton asChild>
+                            <Link to="/" className="[&.active]:font-bold">
+                                <HomeIcon />
+                                <span>Home</span>
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton asChild>
+                            <Link to="/about" className="[&.active]:font-bold">
+                                <NotebookTabs/>
+                                <span>About</span>
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
                 </SidebarGroup>
             </SidebarContent>
             <SidebarFooter />
         </Sidebar>
-        <main className='flex flex-1 min-w-0'>
-            <SidebarTrigger />
+        <main className='flex flex-1 min-w-0 flex-col'>
+            <header className='flex md:hidden'><SidebarTrigger /></header>
             <Outlet />
             <TanStackRouterDevtools />
         </main>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,7 +12,7 @@ function Index() {
     return (
         <div className='p-4 flex flex-col gap-4 min-w-full'>
             <h3>Look for new recipes!</h3>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {recipes.map(recipe => (<RecipeCard recipe={recipe}/>))}
             </div>
         </div>
