896d77c97ce555c59533fe2ed470de1c87d7a983
vluv99 <v.luva99@gmail.com>
2026-03-21 15:31:20 +0100

    fix: path of aliases

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import * as path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,4 +14,9 @@ export default defineConfig({
       react(),
       tailwindcss()
   ],
+    resolve: {
+      alias: {
+          '@': path.resolve(__dirname, './src'),
+      }
+    }
 })
